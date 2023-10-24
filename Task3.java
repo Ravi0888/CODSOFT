dbaefb1c1e7ee5cd6a215b71ca5e2067f4fa7a1d
Ravi0888 <148316586+Ravi0888@users.noreply.github.com>
2023-10-24 10:52:57 +0530

    Task 3. Codsoft internship
    My First Internship Tasks of Codsoft as a java intern

---
+++ Task3.java
@@ -0,0 +1,100 @@
+import java.util.*;
+class UserAccount
+{
+	private String name;
+	private String accountNummber;
+	private int balance;
+	
+	public UserAccount(String name,String accountNummber,int balance  )
+	{
+		this.name=name;
+		this.accountNummber=accountNummber;
+		this.balance=balance;
+	}
+    
+	public String name()
+	{
+		return name;
+	}
+	public String accountNummber()
+	{
+		return accountNummber;
+	}
+	public int balance()
+	{
+		return balance;
+	}
+	public void deposite(double amount)
+	{
+		balance+=amount;
+	}
+	public void withdraw(int amount)
+	{
+		if(amount <=balance)
+		{
+			balance-=amount;
+		}
+		else{
+			System.out.println("Insufficent balance");
+		}
+	}
+	
+}
+class ATM 
+{
+public static void main(String ar[])
+{
+	System.out.println();
+	UserAccount user=new UserAccount("Ravi Rajput","54321",10000);
+	System.out.println("Welcome ,"+user.name());
+	while(true)
+	{
+	int withdraw,deposite,checkbalance;
+	System.out.println("Plz enter any number to performs specific operation");
+	System.out.println();
+	System.out.println("1. Check Balance");
+	System.out.println("2. Deposit");
+	System.out.println("3. Withdraw");
+	System.out.println("4. Exit");	
+	System.out.println("");	
+	Scanner sc=new Scanner(System.in);
+    int choice =sc.nextInt();
+	switch (choice)
+	{
+	case 1:
+	System.out.println("your current bank balance is:"+user.balance());
+	System.out.println();
+	break;
+	
+	case 2:
+	System.out.println("Please enter the amount you'd like to deposit:");
+	int deposite_amount=sc.nextInt();
+	user.deposite(deposite_amount);
+	System.out.println("Succesfully deposited :"+(deposite_amount));
+	System.out.println();
+	break;
+	
+	case 3:
+	System.out.println("Please enter the amount you would like to withdraw:");
+	int withdrawal_amount=sc.nextInt();
+	user.withdraw(withdrawal_amount);
+	System.out.println("Succesfully withdraw:"+(withdrawal_amount));
+	System.out.println();
+
+	break;
+	
+	case 4:
+	System.out.println("Thanks you for being here ! good bye");
+	System.exit(0);
+	break;
+	
+	default:
+	System.out.println("Invalid Option! Please try Again");
+	break;
+	}
+	}
+	
+}
+}
+	
+	
